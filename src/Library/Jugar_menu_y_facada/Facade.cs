80faa8a07984499ff9ab42dae1c9b8caf953033e
ElPolisha <lautaro.arrigoni@correo.ucu.edu.uy>
2024-10-10 20:59:34 -0300

    Cambié la Facada

--- src/Library/Jugar_menu_y_facada/Facade.cs
+++ src/Library/Jugar_menu_y_facada/Facade.cs
@@ -1,6 +1,29 @@
 namespace Library;
+using System;
 
-public class Facade
+class Juego
 {
+    public Jugador Jugador{ get; private set; }
+    public Menu Menu { get; private set; }
+    public string Pokemon { get; private set; }
+
+    public void IniciarPartida()
+    {
+        Jugador = new Jugador();
+        Menu = new Menu();
+        Console.WriteLine("¡Partida iniciada!");
+    }
     
-}
+    private string MostrarMenu()
+    {
+        string[] opciones = { "1. Jugar", "2. Salir" };
+        Console.WriteLine("Menú:");
+        foreach (var opcion in opciones)
+        {
+            Console.WriteLine(opcion);
+        }
+        Console.Write("Elige una opción: ");
+        return Console.ReadLine();
+    }
+    }
+

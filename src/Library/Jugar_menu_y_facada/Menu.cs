b9f1c3c3b56a51978e1c29cc41d8193ee1dc68dd
Cuete <adolfo.bravo@correo.ucu.edu.uy>
2024-10-10 20:58:48 -0300

    cambie el menu

--- src/Library/Jugar_menu_y_facada/Menu.cs
+++ src/Library/Jugar_menu_y_facada/Menu.cs
@@ -2,5 +2,20 @@
 
 public class Menu
 {
+    public bool Turno { get; set; }
+    public bool Batalla { get; set; }
+
+    public void Estado_Batalla()
+    {
+        if (Jugador.ListPokemons.Count > 0)
+        {
+            Console.WriteLine($"Al jugador le quedan {Jugador.ListPokemons.Count} pokemones.");
+            Console.WriteLine("Cuando al jugador le queden cero pokemones la batalla finalizará");
+        }
+        else
+        {
+            Console.WriteLine("La batalla finalizó");
+        }
+    }
 
 }

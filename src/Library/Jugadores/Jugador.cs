263582d4aff929732ac270798c2084b6a7513264
Agustin <agustincigaran@icloud.com>
2024-10-11 01:54:34 -0300

    mejore la estetica y cosas basicas

--- src/Library/Jugadores/Jugador.cs
+++ src/Library/Jugadores/Jugador.cs
@@ -13,86 +13,86 @@ public class Jugador
         this.Name = nombre;
         ListPokemons = new List<Pokemon>
         {
-            new Pokemon("Crocalor", 81, 78, "fuego", new List<Ataque>
+            new Pokemon("Crocalor", 81, 78, "Fuego", new List<Ataque>
             {
-                new Ataque("Llamarada", 30),
-                new Ataque("Ascuas", 15),
-                new Ataque("Giro Fuego", 25),
-                new Ataque("Latigo", 10)
+                new Ataque("Explosion de Fuego", 110),
+                new Ataque("Colmillo de Fuego", 65),
+                new Ataque("Carga de Fuego", 50),
+                new Ataque("Lanzallamas", 90)
             }),
-            new Pokemon("juan", 20, 20, "fuego", new List<Ataque>
+            new Pokemon("Cacnea", 50, 40, "Hierba", new List<Ataque>
             {
-                new Ataque("Llamarada", 30),
-                new Ataque("Ascuas", 15),
-                new Ataque("Giro Fuego", 25),
-                new Ataque("Latigo", 10)
+                new Ataque("Bola de Energia", 90),
+                new Ataque("Drenaje", 75),
+                new Ataque("Nudo de Hierba", 45),
+                new Ataque("Tormenta de Hojas", 130)
             }),
-            new Pokemon("clara", 20, 20, "fuego", new List<Ataque>
+            new Pokemon("Dewott", 75, 60, "Agua", new List<Ataque>
             {
-                new Ataque("Llamarada", 30),
-                new Ataque("Ascuas", 15),
-                new Ataque("Giro Fuego", 25),
-                new Ataque("Latigo", 10)
+                new Ataque("Cuchilla de Agua", 70),
+                new Ataque("Chorro de Agua", 40),
+                new Ataque("Marea Alta", 90),
+                new Ataque("Agua Helada", 50)
             }),
-            new Pokemon("armando", 20, 20, "fuego", new List<Ataque>
+            new Pokemon("Gagnar", 60, 60, "Fantasma", new List<Ataque>
             {
-                new Ataque("Llamarada", 30),
-                new Ataque("Ascuas", 15),
-                new Ataque("Giro Fuego", 25),
-                new Ataque("Latigo", 10)
+                new Ataque("Maldicion", 20),
+                new Ataque("Vinculo de Destino", 65),
+                new Ataque("Mal de Ojo", 65),
+                new Ataque("Sombra Nocturna", 40)
             }),
-            new Pokemon("nati", 20, 20, "fuego", new List<Ataque>
+            new Pokemon("Mareep", 55, 40, "Electrico", new List<Ataque>
             {
-                new Ataque("Llamarada", 30),
-                new Ataque("Ascuas", 15),
-                new Ataque("Giro Fuego", 25),
-                new Ataque("Latigo", 10)
+                new Ataque("Descarga", 80),
+                new Ataque("Trueno", 75),
+                new Ataque("Rayo", 90),
+                new Ataque("Choque Relampago", 110)
             }),
         };
-        Console.WriteLine($"\nSu jugador se llama {this.Name}\n");
+        Console.WriteLine($"\n 🗿 Su jugador se llama {this.Name}\n");
     }
 
     public Pokemon Seleccionar_Pokemons()
     {
         List<string> actuales = new List<string>();
         
-        Console.WriteLine($"Estos son sus Pokémons actuales:");
+        Console.WriteLine($" ◽ {this.Name}\n 🐗 Estos son sus Pokémons actuales:");
         
         foreach (Pokemon bicho in ListPokemons)
         {
             if (!bicho.EnCombate)
             {
                 actuales.Add(bicho.Name);
-                Console.WriteLine($"✪ {bicho.Name}");
+                Console.WriteLine($" ✪ {bicho.Name}");
             }
         }
 
         if (actuales.Count == 0)
         {
-            Console.WriteLine("No tienes mas pokemons disponibles para luchar");
+            Console.WriteLine($" ❌ {this.Name}\nNo tienes mas pokemons disponibles para luchar");
             return null;
         }
         
-        Console.WriteLine($"\nEscriba a quien quiere usar: ");
+        Console.WriteLine($"\n ⏳ {this.Name}\n Escriba a quien quiere usar: ");
         string seleccionado = Console.ReadLine();
         
         Pokemon encontrado = ListPokemons.Find(p => p.Name == seleccionado);
         
         if (encontrado != null)
         {
-            Console.WriteLine($"\n{this.Name} saco a {seleccionado}\n");
-            Console.WriteLine($"{encontrado.Name} tiene {encontrado.Hp} puntos de vida, {encontrado.Defensa} puntos de defensa y es de tipo {encontrado.Tipo}\n");
-            Console.WriteLine("Ataques disponibles: ");
+            Console.WriteLine($"\n 🐵 {this.Name} saco a {seleccionado}\n");
+            Console.WriteLine($" 🐵 {encontrado.Name} tiene {encontrado.Hp} puntos de vida, {encontrado.Defensa} puntos de defensa y es de tipo {encontrado.Tipo}\n");
+            Console.WriteLine(" 💣 Ataques disponibles: ");
             foreach (Ataque ataq in encontrado.Ataques)
             {
                 Console.WriteLine($"- {ataq.Name} = {ataq.Daño}");
             }
-            Console.WriteLine("\n")
+            Console.WriteLine("\n");
             return encontrado;
         }
         else
         {
-            Console.WriteLine("Usted no tiene ese pokemon, seleccione otro.");
+            Console.WriteLine($" 🚫 {this.Name}\n Usted no tiene ese pokemon, seleccione otro.");
             return Seleccionar_Pokemons();
         }
     }
@@ -101,35 +101,46 @@ public class Jugador
     {
         public static void IniciarBatalla(Jugador jugador1, Jugador jugador2)
         {
-            Console.WriteLine("Se da inicio a la batalla pokemon\n");
-
             Pokemon pokemon1 = jugador1.Seleccionar_Pokemons();
             Pokemon pokemon2 = jugador2.Seleccionar_Pokemons();
+            
+            Console.WriteLine(" 🎮 Quiere iniciar la batalla pokemon (Si/No): ");
+            string opcion = Console.ReadLine();
 
-            if (pokemon1 == null || pokemon2 == null)
+            if (opcion == "Si")
             {
-                Console.WriteLine("No hay suficientes pokemons para luchar");
+                while (pokemon1.Hp > 0 && pokemon2.Hp > 0)
+                {
+                    pokemon1.Luchar(pokemon2);
+                    if (pokemon2.Hp <= 0)
+                    {
+                        Console.WriteLine($"\n 🌟 {pokemon2.Name} fue derrotado, {jugador1.Name} gana la batalla 🌟 ");
+                        break;
+                    }
+
+                    pokemon2.Luchar(pokemon1);
+                    if (pokemon1.Hp <= 0)
+                    {
+                        Console.WriteLine($"\n 🌟 {pokemon1.Name} fue derrotado {pokemon2.Name} gana la batalla 🌟 ");
+                        break;
+                    }
+                }
+                pokemon1.EnCombate = false;
+                pokemon2.EnCombate = false;
+            }
+            else if (opcion == "No")
+            {
+                if (pokemon1 == null || pokemon2 == null)
+                {
+                    Console.WriteLine(" 🔎 No hay suficientes pokemons para luchar");
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine(" 🚫 No selecciono una opcion valida.");
                 return;
             }
-            
-            while (pokemon1.Hp > 0 && pokemon2.Hp > 0)
-            {
-                pokemon1.Luchar(pokemon2);
-                if (pokemon2.Hp <= 0)
-                {
-                    Console.WriteLine($"{pokemon2.Name} fue derrotado, {jugador1.Name} gana la batalla");
-                    break;
-                }
-
-                pokemon2.Luchar(pokemon1);
-                if (pokemon1.Hp <= 0)
-                {
-                    Console.WriteLine($"{pokemon1.Name} fue derrotado {pokemon2.Name} gana la batalla");
-                    break;
-                }
-            }
-            pokemon1.EnCombate = false;
-            pokemon2.EnCombate = false;
         }
     }
 }
--- src/Library/Pokemones/Pokemon.cs
+++ src/Library/Pokemones/Pokemon.cs
@@ -23,7 +23,7 @@ public class Pokemon
     {
         if (this.Hp <= 0)
         {
-            Console.WriteLine($"{this.Name} no puede luchar, ya que no tiene puntos de vida.");
+            Console.WriteLine($" 💀 {this.Name} no puede luchar, ya que no tiene puntos de vida.");
             return;
         }
         
@@ -33,18 +33,18 @@ public class Pokemon
 		Random rnd = new Random();
 		Ataque ataqueUsado = this.Ataques[rnd.Next(this.Ataques.Count)];
 
-		Console.WriteLine($"{this.Name} uso el ataque {ataqueUsado.Name} contra {oponente.Name}");
+		Console.WriteLine($" 👊 {this.Name} uso el ataque {ataqueUsado.Name} contra {oponente.Name}");
 
 		int dañoFinal = Math.Max(0, ataqueUsado.Daño - oponente.Defensa);
 		oponente.Hp -= dañoFinal;
 		//////////////
 
-        Console.WriteLine($"{oponente.Name} recibe {dañoFinal} de daño. Le quedan {oponente.Hp} de vida.");
+        Console.WriteLine($" 💥 {oponente.Name} recibe {dañoFinal} de daño. Le quedan {oponente.Hp} de vida.");
 
         if (oponente.Hp <= 0)
         {
             oponente.Hp = 0;
-            Console.WriteLine($"{oponente.Name} fue derrotado");
+            Console.WriteLine($" 😵 {oponente.Name} fue derrotado");
             oponente.EnCombate = false;
         }
     }
@@ -54,26 +54,26 @@ public class Pokemon
         switch (objeto.ToLower())
         {
             case "pocion":
-                Console.WriteLine($"{this.Name} uso pocion y recupero 20 puntos de vida");
+                Console.WriteLine($" 💝 {this.Name} uso pocion y recupero 20 puntos de vida");
                 this.Hp += 20;
                 break;
             case "antidoto":
-                Console.WriteLine($"{this.Name} uso antidoto");
+                Console.WriteLine($" 💉 {this.Name} uso antidoto");
                 break;
             case "revivir":
                 if (this.Hp <= 0)
                 {
-                    Console.WriteLine($"{this.Name} fue revivido y tiene 50 puntos de vida");
+                    Console.WriteLine($" 😇 {this.Name} fue revivido y tiene 50 puntos de vida");
                     this.Hp = 50;
                 }
                 else
                 {
-                    Console.WriteLine("Solo puedes revivir a un pokemon que no tiene puntos de vida");
+                    Console.WriteLine(" 😅 Solo puedes revivir a un pokemon que no tiene puntos de vida");
                 }
 
                 break;
             default:
-                Console.WriteLine($"El objeto {objeto} no esta disponible en la mochila");
+                Console.WriteLine($" 🚫 El objeto {objeto} no esta disponible en la mochila");
                 break;
         }
 

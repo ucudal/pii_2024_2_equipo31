59e3e12ac76198412e067b9fa7ff072af2d1e488
Agustin <agustincigaran@icloud.com>
2024-10-10 21:28:35 -0300

    cree el codigo de las clases pokemon, jugador y probe el program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace Program;
+﻿using Library;
+using System;
+
+namespace Program;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Jugador feijoada = new Jugador("Agustin");
+        feijoada.Seleccionar_Pokemons();
+        
     }
 }

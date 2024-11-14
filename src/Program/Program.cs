47f833736af7f1accd737f38489ec195d8b240b4
Agustin <agustincigaran@icloud.com>
2024-11-14 17:45:01 -0300

    probe 2 test, cree la clase bot

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,17 +1,33 @@
 ﻿using Library;
 using System;
+using Discord;
 using DSharpPlus;
-using DSharpPlus.CommandsNext;
+using Discord.WebSocket;
 using DSharpPlus.EventArgs;
+using DSharpPlus.CommandsNext;
+using System.Threading.Tasks;
+using TokenType = Discord.TokenType;
 
 namespace Program;
 
 class Program
 {
+    
+    private static async Task Main(string[] args)
+    {
+        var bot = new Bot();
+        await bot.StartAsync();
+
+        await Task.Delay(-1);
+    }
+    
+    /*
     static void Main()
     {
+
         Console.WriteLine("\n ⏩  Bienvenido a PokeWorld ⏪  ");
         Menu menujugar = new Menu();
         menujugar.MostrarMenuPrincipal();
     }
+    */
 }

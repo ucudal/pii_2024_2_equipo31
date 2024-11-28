5f75e0832962c3847cb509f840734c4e34d00b47
Cuete <adolfo.bravo@correo.ucu.edu.uy>
2024-11-28 20:20:17 -0300

    Entrega Defensa

---
+++ src/Library/defensa.cs
@@ -0,0 +1,37 @@
+namespace Library;
+
+public class defensa
+{
+    public class Jugador
+    {
+        public Pokemon pokemonEnBatalla { get; private set; }
+        public List<Pokemon> ListPokemons { get; set; }
+        foreach list<Pokemon> ListPokemons
+    }
+
+    public void SeleccionarPokemonParaLuchar()
+    {
+        Jugador jugador = new Jugador("Ash");
+
+        public void JugadorTienePokemonsDisponibles()
+        {
+            Jugador jugador = new Jugador("Ash");
+            jugador.ListPokemons.Add(new Pokemon(1, "Pikachu", 80, 45, "eléctrico", new List<IAtaque>()));
+            jugador.ListPokemons.Add(new Pokemon(2, "Charmander", 70, 50, "fuego", new List<IAtaque>()));
+
+        }
+
+        // Assert
+             
+        
+    }
+}
+class Defensaa{}
+    static void Main(string[]args){
+        string[,] efectividad={
+            {"Fuego", "Hierba","Super Efectivo"}
+            {"Hierba","Agua", "Super Efectivo"}
+            {"Agua", "Electrico", "Super Efectivo"}
+            {"Hierba", "Fuego","Nada Efectivo"}
+}
+}
